74bcbe0b238768e757b062cbf5c70d7bdeef4685
hatsu38 <hajiwata0308@gmail.com>
2023-04-25 14:35:15 +0900

    Initial commit from Create Next App

---
+++ src/pages/_app.tsx
@@ -0,0 +1,6 @@
+import '@contracter/src/styles/globals.css'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
