932a3a6b6ff652bb4b715d494a6023188abf158e
hatsu38 <hajiwata0308@gmail.com>
2023-04-25 15:36:42 +0900

    feat: 🎸 Add Top Page features

---
+++ src/features/home/components/HomeLayout.tsx
@@ -0,0 +1,9 @@
+export const HomeLayout = () => {
+  return (
+    <main
+      className={"m-8 flex min-h-screen flex-col items-center justify-between p-24"}
+    >
+      <h1 className="text-red">PDF読み込み</h1>
+    </main>
+  )
+};
