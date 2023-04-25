861f822e02e99ba7a7123c1b9b40b41c7b81e318
hatsu38 <hajiwata0308@gmail.com>
2023-04-25 23:21:57 +0900

    style: 💄 tailwindにprimary-colorを設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,20 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: {
+          50: "#f2f9f9",
+          100: "#ddeff0",
+          200: "#bfe0e2",
+          300: "#92cace",
+          400: "#5faab1",
+          500: "#438e96",
+          600: "#3b757f",
+          700: "#356169",
+          800: "#325158",
+          900: "#2d464c",
+        },
+      },
     },
   },
   plugins: [],
